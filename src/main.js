a904a7357f5f4d93c0d9a7c298d5d54497e49d27
Rishabh Baid <rishabhbaid1@gmail.com>
2019-01-31 16:20:41 +0000

    Merged in feature/sentry-integration (pull request #28)
    Changes for Sentry integration.
    
    Approved-by: Samarth Mathur <samarth.math@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './assets/css/main.css'
+import * as Sentry from '@sentry/browser'
+
+// Login at https://sentry.io to see errors in production.
+// Username: abhivyaktcom@gmail.com
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({
+        dsn: 'https://6ee2420664804c8089f8d5bfb1f397b6@sentry.io/1383381',
+        integrations: [new Sentry.Integrations.Vue({ Vue })]
+    })
+}
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
