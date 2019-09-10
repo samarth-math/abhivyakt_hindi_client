fcc6c5e3442ecac722f7a3dba0162f43d6605c20
Samarth Mathur <samarth.math@gmail.com>
2019-09-10 17:56:51 +0000

    Merged in improvements/authorWorks (pull request #56)
    Rachnakar works only shows the kind of works available for the Rachnakar
    
    * Fixes syntax error
    
    * Rachnakar works only shows the kind of works available for the Rachnakar
    
    Approved-by: Rishabh Baid <rishabhbaid1@gmail.com>

--- src/components/RachanakarComponents/RachnakarPage.vue
+++ src/components/RachanakarComponents/RachnakarPage.vue
@@ -1,17 +1,18 @@
 <template>
     <div v-if="isContentLoaded">
-        <home-page-header
-         :headerText="authorName" />
-        <img 
-        v-if="loadedContent.image"
-        :src="'data:image/jpeg;base64,' + imageHex"/>
-        <br/>
+        <home-page-header :headerText="authorName" />
+        <img
+            v-if="loadedContent.image"
+            :src="'data:image/jpeg;base64,' + imageHex"
+        />
+        <br />
         {{loadedContent.biography}}
         <base-page-section section-header="अक्षरों से रचनाकार ब्राउज़ करें">
-        <lettered-paginated-table
-        :from-location="rachnakarContentApiEndpoint"
-        :tabs="tabObject"
-        :object-column-table-column-mapping="{'नाम':'title'}"/>
+            <lettered-paginated-table
+                :from-location="rachnakarContentApiEndpoint"
+                :tabs="tabObject"
+                :object-column-table-column-mapping="{'नाम':'title'}"
+            />
         </base-page-section>
     </div>
 </template>
@@ -25,40 +26,42 @@ import featuredObject from '../../mixins/featuredObject.js'
 import featuredImage from '../../mixins/featuredImage.js'
 import BasePageSection from "../BaseComponents/BasePageSection"
 import LetteredPaginatedTable from "../BaseComponents/LetteredPaginatedTable"
-import {ObjectHindiEnum} from "../../mixins/staticVariables.js"
+import { ObjectHindiEnum } from "../../mixins/staticVariables.js"
 
 export default {
     name: "RachnakarPage",
-    data(){
-        return {
-            tabObject: {
-                [ObjectHindiEnum.kavita]:"",
-                [ObjectHindiEnum.kahani]:"",
-                [ObjectHindiEnum.dohe]:""
-            }
-        }
-    },
-    props: { 
+    props: {
         'id': { // Getting this from the route, not defined manually
             type: String,
             required: true
         },
     },
     computed: {
-        'authorName' : function() {
+        'authorName': function () {
             return this.loadedContent.name
         },
-        'imageHex' : function() {
-            if(this.loadedContent.image){
+        'imageHex': function () {
+            if (this.loadedContent.image) {
                 this.fetchImage(this.loadedContent.image.thumbnail)
             }
             return this.image
         },
-        rachnakarContentApiEndpoint: function() {
+        rachnakarContentApiEndpoint: function () {
             return "rachnakar/" + this.id + "/content"
+        },
+        tabObject: function () {
+            var tab = {}
+            if (this.isContentLoaded) {
+                if (this.loadedContent.works) {
+                    for (var work in this.loadedContent.works) {
+                        tab[ObjectHindiEnum[work]] = ""
+                    }
+                }
+            }
+            return tab
         }
     },
-    mixins:[featuredObject, featuredImage],
+    mixins: [featuredObject, featuredImage],
     components: {
         HomePageHeader,
         LetteredPaginatedTable,
